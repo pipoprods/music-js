bf34dfee58eb84ba85312ca1465eed0426f70b4d
Sébastien NOBILI <pipoprods@free.fr>
2019-02-11 13:17:23 +0100

    Sanitize MPD request arguments & get MPD connection parameters from environment

--- src/services/mpc.ts
+++ src/services/mpc.ts
@@ -8,7 +8,7 @@ export class Mpc {
 
     constructor(host?: string, port?: number) {
         // Connect to server
-        this.mpc = new mpd.MPDClient(host || 'localhost', port || 6600);
+        this.mpc = new mpd.MPDClient(host || process.env.MPD_HOST || 'localhost', port || process.env.MPD_PORT || 6600);
     }
 
     // Log errors
@@ -35,16 +35,16 @@ export class Mpc {
     }
     // Get artist albums
     public async albums(artist: string): Promise<Album[]> {
-        const cmd = new mpd.MPDCommand(`list album "${artist}"`);
+        const cmd = new mpd.MPDCommand(`list album "${this.sanitizeArg(artist)}"`);
         let albums: Album[] = await this.mpc.execute(cmd)
             .then(response => { return this.forceArray(response.response[0].Album).map(a => Object.assign(new Album(), { name: a })); })
             .catch(error => this.error(error));
 
         // Get album dates
         for (let index = 0; index < albums.length; index++) {
-            albums[index].year = await this.mpc.execute(new mpd.MPDCommand(`list date artist "${artist}" album "${albums[index].name}"`))
+            albums[index].year = await this.mpc.execute(new mpd.MPDCommand(`list date artist "${this.sanitizeArg(artist)}" album "${this.sanitizeArg(albums[index].name)}"`))
                 .then(response => {
-                    return this.forceArray(response.response[0].Date)[0];
+                    return this.forceArray(response.response[0] ? response.response[0].Date : undefined)[0];
                 })
                 .catch(error => this.error(error));
         }
@@ -55,5 +55,9 @@ export class Mpc {
     // Ensure data is an array
     private forceArray(data) {
         return data instanceof Array ? data : [ data ];
+    }
+
+    private sanitizeArg(arg: string): string {
+        return arg.replace(/"/g, '\\"');
     }
 }
